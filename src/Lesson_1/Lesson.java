f641a262212effef077c7e0763d0fd01a5c7574c
jenya <jenya@MacBook-Air-jenya.local>
2022-06-07 02:10:18 +0300

    level 2 is done

--- src/Lesson_1/Lesson.java
+++ src/Lesson_1/Lesson.java
@@ -2,15 +2,18 @@ package Lesson_1;
 
 
 import java.io.PrintStream;
+import java.util.Arrays;
 
 public class Lesson {
     public static void main(String[] args) {
         //Базовый уровень
         //Задача №1
         //Дано (их менять нельзя)
+
         String hi = "                Hello ";
         String world = " WoRld!";
         char newLine = '\n';
+
         //Создать из трех переменных единую строку,
         //Привести к правильному виду (Ниже)
         //затроить (Можно вызвать тольку одну команду System.out.print())
@@ -20,13 +23,10 @@ public class Lesson {
         //Hello world!
         //Hello world!
 
-        String stringTrim = (hi.trim() + world) + (newLine);
+        String stringTrim = hi.trim() + world + newLine;
         String stringRepeat = stringTrim.repeat(3);
 
         System.out.println(stringRepeat);
-
-
-
 
         //Задача №2
         //Создать переменные с ростом, весом.
@@ -34,17 +34,41 @@ public class Lesson {
         //Пример результата вывода на экран:
         //21.0
 
+        double height = 190;
+        double weight = 113;
+        double bmi = (weight / (Math.pow(height, 2)));
+
+        System.out.println(Math.floor(bmi * 10000) + "\n");
+
         //Задача №3
         //Создать из массива букв a,b,c,d,e, строку,вывести на экран , поменять в массиве 4 букву по счету на h,
         //и снова создать строку, вывести на экран
+
+        String[] string = {"a", "b", "c", "d", "e"};
+        System.out.println(Arrays.toString(string));
+        string[3] = "h";
+        System.out.println(Arrays.toString(string) + "\n");
 
 
         //Продвинутый уровень
         //Задача №1
         //Произвести преобразование "234" в число типа int и прибавить к этому числу длину строки "some_text"
 
+        String nums = "234";
+        String text = "some_text";
+        int someTextLength = text.length();
+        int numsNumEquivalent = Integer.parseInt(nums);
+
+        System.out.println(numsNumEquivalent + someTextLength + "\n");
+
         //Задача №2
         //Посчитать (a+b)^2 = ?, при a=3, b=5
+
+        int a = 3;
+        int b = 5;
+        int sum = 8;
+        System.out.println((int) Math.pow(sum, 2) + "\n");
+
 
         //Задача №3
         //Создать два массив чисел:
@@ -58,8 +82,38 @@ public class Lesson {
         //(первый массив - 1,2,5,7,10), (второй массив - 2,3,2,17,15),
         //(результат перемножения - (1*2), (2*3), (5*2), (7*17), (10*15)
 
+
+        int[] nums1 = {1, 2, 5, 7, 10};
+        int[] nums2 = {2, 3, 2, 17, 15};
+        int[] numsFusion = new int[nums1.length + nums2.length]; // length = 10
+        int[] numsMultiplied = new int[nums1.length]; // length = 5
+        int[] numsResult = new int[numsFusion.length + nums1.length]; // length = 15;
+
+        System.arraycopy(nums1, 0, numsFusion, 0, nums1.length);
+        System.arraycopy(nums2, 0, numsFusion, nums1.length, nums2.length);
+        System.arraycopy(numsFusion, 0, numsFusion, 0, numsFusion.length);
+
+        for (int i = 0; i < nums1.length; i++) numsMultiplied[i] = nums1[i] * nums2[i];
+
+        System.arraycopy(numsFusion, 0, numsResult, 0, numsFusion.length);
+        System.arraycopy(numsMultiplied, 0, numsResult, numsFusion.length, numsMultiplied.length);
+        System.arraycopy(numsResult, 0, numsResult, 0, numsResult.length);
+
+        System.out.println(Arrays.toString(numsResult) + "\n");
+
+
         //Задача №4
         //В слове "Hello world!" заменить все l на r, сделать все буквы заглавными, выбрать первые 8 символов
+
+        String helloWorld = "Hello world";
+        String newHelloWorld = helloWorld.replace("l", "r");
+        System.out.println(newHelloWorld.toUpperCase());
+
+        // не понял, что такое - "выбрать первые 8 символов"
+
+
+
+
 
         //Экспертный уровень
         //Задача №1
@@ -81,6 +135,4 @@ public class Lesson {
 
 
     }
-
-
 }
